b6450b65893096354e185982df59ec9a505ed4ea
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-03-11 16:09:13 +0000

    Compare with solutions' palindrome implementation

--- recursion_quiz/palindrome.rb
+++ recursion_quiz/palindrome.rb
@@ -4,24 +4,42 @@
 #     string.downcase == string.downcase.reverse
 # end
 
-def palindrome?(string, str_length)
+# def palindrome?(string, str_length)
 
-    if string[str_length - 1] == string[-str_length]
+#     if string[str_length - 1] == string[-str_length]
 
-        if (str_length - 2) == -1
+#         if (str_length - 2) == -1
 
-            p "Last"
-            return true
-        end
+#             return true
+#         end
 
-        palindrome?(string, str_length - 1)
+#         palindrome?(string, str_length - 1)
+#     else
+#         return false
+
+#     end
+# end
+
+# p palindrome?("racecar", "racecar".length)
+
+# p palindrome?("bye", "bye".length)
+
+
+#  Solution:
+
+def palindrome(string)
+    if string.length == 1 || string.length == 0
+      true
     else
-        return false
-
+      if string[0] == string[-1]
+        palindrome(string[1..-2])
+      else
+        false
+      end
     end
-end
+  end
 
-p palindrome?("racecar", "racecar".length)
 
-p palindrome?("bye", "bye".length)
+p palindrome("racecar")
 
+p palindrome("bye")
