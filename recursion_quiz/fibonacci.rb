5ab460f17e9620494aac16cbd56b84da030ea9ac
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-03-11 16:17:41 +0000

    Implement recursive fibonacci function

---
+++ recursion_quiz/fibonacci.rb
@@ -0,0 +1,17 @@
+# Define a recursive function that takes an argument n 
+#  and returns the fibonacci value of that position. 
+#  The fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21... 
+#  So fib(5) should return 5 and fib(6) should return 8.
+
+def fib(n)
+    if n == 0
+        return 0
+    elsif n == 1
+        return 1
+    else 
+        return (fib(n - 1) + fib(n - 2))
+    end
+end
+
+p fib(5)
+p fib(6)
