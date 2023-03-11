4e00ae5f882c469d13a9df63a0b0d86908513110
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-03-11 16:18:40 +0000

    Compare with solutions' function implementation

--- recursion_quiz/fibonacci.rb
+++ recursion_quiz/fibonacci.rb
@@ -5,11 +5,11 @@
 
 def fib(n)
     if n == 0
-        return 0
+        0
     elsif n == 1
-        return 1
+        1
     else 
-        return (fib(n - 1) + fib(n - 2))
+        (fib(n - 1) + fib(n - 2))
     end
 end
 
