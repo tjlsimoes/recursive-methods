e87cfa3d1b95ad8fd708c6eb455ee2a969e9dfa8
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-03-17 17:45:45 +0000

    Implement fibs_rec method

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -18,3 +18,19 @@ def fibs(number)
 end
 
 p fibs(8)
+
+
+def fibs_rec(number, array = [])
+
+    if number == 1
+        return array << 0 
+    elsif number == 2
+        return array << 1
+    elsif number > 2 
+        return array << fibs_rec(number - 1, array) + fibs_rec(number - 2, array) if number - 2 >= 0
+    end
+
+    array
+end
+
+fibs_rec(8)
