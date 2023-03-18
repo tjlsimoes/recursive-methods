52e9158838ebf4761d1787a0601cc6e5fb474eb0
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-03-18 16:38:00 +0000

    Compare merge_sort with Ganthology solution

--- merge_sort/merge_sort.rb
+++ merge_sort/merge_sort.rb
@@ -43,3 +43,30 @@ end
 
 
 merge_sort([5, 2, 1, 3, 6, 4])
+
+
+# Ganthology solution: https://github.com/Ganthology/ruby_recursion/blob/main/lib/merge_sort.rb
+
+def merges_sort(array)
+    # base case
+    return array if array.length < 2
+  
+    middle = array.length / 2
+    # sort left
+    left_half = merge_sort(array[0...middle])
+    # sort right
+    right_half = merge_sort(array[middle..array.length])
+    # merge two halves(sort)
+  
+    sorted = []
+    # compare until one half is empty
+    until left_half.empty? || right_half.empty?
+      # one pointer in each half comparing their current values
+      # smaller values pushed to the sorted array
+      left_half.first <= right_half.first ? sorted << left_half.shift : sorted << right_half.shift
+    end
+  
+    sorted + left_half + right_half
+  end
+  
+  p merges_sort([3, 4, 2, 1, 5, 0, 10, 9, 7, 8, 6])
